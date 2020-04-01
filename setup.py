6fd538f2ba25adbc242f2d0246f5d9719640f54a
kbn-gh <kbn.git@gmail.com>
2020-04-01 11:59:55 -0500

    added sklearn and xgboost to setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
    download_url="https://github.com/hengenlab/musclebeachtools_hlab",
    packages=['musclebeachtools'],
    install_requires=['ipython', 'numpy', 'matplotlib', 'seaborn', 'pandas',
-                     'scipy'],
+                     'scipy', 'sklearn', 'xgboost'],
    classifiers=[
         'Development Status :: 1 - Pre-Alpha',
         'Natural Language :: English',
