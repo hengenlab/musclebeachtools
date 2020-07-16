7aa605b06d8affb91f4c7835f30c183e5a23087b
kbn-gh <kbn.git@gmail.com>
2020-07-16 11:40:02 -0500

    added cell_isi_hist, to generate isi spiketimes not part of neuron class

--- musclebeachtools/mbt_neurons.py
+++ musclebeachtools/mbt_neurons.py
@@ -2629,6 +2629,86 @@ def load_spike_waveforms_be(neuron_list, file_name):
     return neuron_list
 
 
+def cell_isi_hist(time_s, start, end, isi_thresh=0.1,
+                  nbins=101, lplot=1):
+
+    '''
+    Return a histogram of the interspike interval (ISI) distribution.
+    This is typically used to evaluate whether a spike train exhibits
+    a refractory period and is thus consistent with a
+    single unit or a multi-unit recording.
+    This function will plot the bar histogram of that distribution
+    and calculate the percentage of ISIs that fall under 2 msec.
+
+    cell_isi_hist(time_s, start=False, end=False, isi_thresh=0.1,
+                  nbins=101, lplot=1)
+
+    Parameters
+    ----------
+    time_s : time in seconds
+    start : Start time
+    end : End time
+    isi_thresh : isi threshold (default 0.1)
+    nbins : Number of bins (default 101)
+    lplot : To plot or not (default lplot=1, plot isi)
+
+    Returns
+    -------
+    ISI : spike time difference (a[i+1] - a[i]) along axis
+
+    Raises
+    ------
+
+    See Also
+    --------
+
+    Notes
+    -----
+
+    Examples
+    --------
+    cell_isi_hist(time_s, start, end, isi_thresh=0.1, nbins=101, lplot=1)
+
+    '''
+
+    # For a view of how much this cell is like a "real" neuron,
+    # calculate the ISI distribution between 0 and 100 msec.
+
+    logger.info('Calculating ISI')
+
+    logger.debug('start and end is %s and %s', start, end)
+
+    # Calulate isi
+    idx = np.where(np.logical_and(time_s >= start, time_s <= end))[0]
+    ISI = np.diff(time_s[idx])
+
+    # plot histogram and calculate contamination
+    edges = np.linspace(0, isi_thresh, nbins)
+    hist_isi = np.histogram(ISI, edges)
+
+    # Calculate contamination percentage
+    contamination = 100*(sum(hist_isi[0][0:int((0.1/isi_thresh) *
+                         (nbins-1)/50)])/sum(hist_isi[0]))
+    contamination = round(contamination, 2)
+    cont_text = 'Contamination is {} percent.' .format(contamination)
+    logger.info('Contamination is {} percent.' .format(contamination))
+
+    if lplot:
+        plt.ion()
+        with sns.axes_style("white"):
+            fig1 = plt.figure()
+            ax = fig1.add_subplot(111)
+            # ax.bar(edges[1:]*1000-0.5, hist_isi[0], color='#6a79f7')
+            ax.bar(edges[1:]*1000-0.5, hist_isi[0], color='#0b559f')
+            ax.set_ylim(bottom=0)
+            ax.set_xlim(left=0)
+            ax.set_xlabel('ISI (ms)')
+            ax.set_ylabel('Number of intervals')
+            ax.text(30, 0.7*ax.get_ylim()[1], cont_text)
+        sns.despine()
+    return ISI, edges, hist_isi
+
+
 # loading function
 def ksout(datadir, filenum=0, prbnum=1, filt=None):
     '''
