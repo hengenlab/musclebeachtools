67a332faaa45d2ea06f46ec10215cd5799c6f46a
kbn-gh <kbn.git@gmail.com>
2021-02-24 11:48:00 -0600

    commented for testing init

--- musclebeachtools/__init__.py
+++ musclebeachtools/__init__.py
@@ -1,8 +1,8 @@
-import subprocess
-try:
-    process = subprocess.check_output(['git', 'describe'], shell=False)
-    __git_version__ = rocess.strip().decode('ascii')
-except Exception as e:
-    __git_version__ = 'unknown'
+#import subprocess
+#try:
+#    process = subprocess.check_output(['git', 'describe'], shell=False)
+#    __git_version__ = process.strip().decode('ascii')
+#except Exception as e:
+#    __git_version__ = 'unknown'
 from .mbt_neurons import *
 from .mbt_spkinterface_out import *
