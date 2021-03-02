4072b7e38e6a07eb51251dc37568ff459c0f962b
kbn-gh <kbn.git@gmail.com>
2021-03-02 13:22:09 -0600

    __git_version__ field

--- musclebeachtools/__init__.py
+++ musclebeachtools/__init__.py
@@ -1,8 +1,6 @@
-#import subprocess
-#try:
-#    process = subprocess.check_output(['git', 'describe'], shell=False)
-#    __git_version__ = process.strip().decode('ascii')
-#except Exception as e:
-#    __git_version__ = 'unknown'
+try:
+    from ._version import __git_version__
+except Exception as e:
+    pass
 from .mbt_neurons import *
 from .mbt_spkinterface_out import *
