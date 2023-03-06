50063837bdccf57e36e1f7a839a2ab608f725b89
kbn-gh <kbn.git@gmail.com>
2023-03-06 12:22:04 -0600

    updated tag

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except Exception as e:
 
 setup(
    name='musclebeachtools',
-   version='1.2.0',
+   version='1.2.1',
    description='This package used to analyze neuron data, from basic plotting\
                (ISI histograms, firing rate, cross corr), to hard core\
                computational work (ising models, branching ratios, etc).',
