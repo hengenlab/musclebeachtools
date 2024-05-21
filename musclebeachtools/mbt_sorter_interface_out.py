86b86362a6d268e30bbf5648c1fb4cba7751d254
kbn-gh <kbn.git@gmail.com>
2024-05-21 13:41:10 -0500

    added function ms5out

--- musclebeachtools/mbt_sorter_interface_out.py
+++ musclebeachtools/mbt_sorter_interface_out.py
@@ -2,8 +2,8 @@ import glob
 import os
 import os.path as op
 import pickle
-# import numpy as np
-# import neuraltoolkit as ntk
+import numpy as np
+import neuraltoolkit as ntk
 import musclebeachtools as mb
 
 
@@ -31,6 +31,276 @@ def get_group_value(channel, group_size):
         raise ValueError("Group size must not be zero or negative")
 
     return int(channel % group_size)
+
+
+def ms5out(sorted_data, noflylist, rec_time,
+           file_datetime_list, ecube_time_list,
+           wfs=None,
+           peak_channels=None,
+           amps=None,
+           wf_b=None, wf_e=None,
+           filt=None,
+           t_ch_size=None,
+           model_file='/media/HlabShare/models/xgboost_autoqual_prob',
+           sex=None, birthday=None, species=None,
+           animal_name=None,
+           region_loc=None,
+           genotype=None,
+           expt_cond=None,
+           lskipautoqual=None,
+           min_amps=10):
+    '''
+    function to load neuron objects from the spike interface output
+
+    Parameters
+    ----------
+    datadir : Location of output files
+    filenum : File number if there is many blocks (default 0)
+    prbnum : Probe number (default 1). Range 1-10.
+    filt : filter by quality. filt=[1], loads only quality 1 neurons.
+
+
+    Returns
+    -------
+    n1 : All neurons as a list. For example n1[0] is first neuron.
+
+    Raises
+    ------
+
+    See Also
+    --------
+
+    Notes
+    -----
+
+    Examples
+    --------
+    datadir = "/hlabhome/kiranbn/Animalname/final/"
+    n1 = ksout(datadir, filenum=0, prbnum=1, filt=[1, 3])
+
+
+    '''
+
+    # filt to empty list
+    print("dir sorted_data ", dir(sorted_data), flush=True)
+    print(f'amps {amps}')
+    amps = amps[0]
+    amps_keys = list(amps.keys())
+    if filt is None:
+        filt = []
+
+    # print("Finding unit ids")
+    unique_clusters = sorted_data.get_unit_ids()
+
+    # Sampling rate
+    # print('Finding sampling rate')
+    # mb.Neuron.fs = sorted_data.get_sampling_frequency()
+    fs = sorted_data.get_sampling_frequency()
+    print("Sampling frequency ", fs)
+    n = []
+
+    # Start and end time
+    # print('Finding start and end time')
+    # start_time = raw_data_start
+    # end_time = raw_data_end
+    # # Convert to seconds
+    # end_time = (np.double(np.int64(end_time) - np.int64(start_time))/1e9)
+    # # reset start to zero
+    # start_time = np.double(0.0)
+    # print((end_time - start_time))
+    # assert (end_time - start_time) > 1.0, \
+    #     'Please check start and end time is more than few seconds apart'
+    # print('Start and end times are %f and %f', start_time, end_time)
+
+    # mb.Neuron.start_time = 0.0
+    start_time = 0.0
+
+    end_time = rec_time / fs
+    print("Start time ", start_time, " end time ", end_time)
+
+    # Loop through unique clusters and make neuron list
+    for unit_idx, unit in enumerate(unique_clusters):
+        # ch_group = sorted_data.get_unit_property(unit, "group")
+        # print("Total i ", i, " unit ", unit)
+        if unit_idx not in noflylist:
+            # print("qual ", cluster_quals[i])
+            if len(filt) == 0:
+                # this is the unit number for indexing spike times
+                # and unit properties
+                sp_c = [unit]
+                # these are spike times
+                sp_t = sorted_data.get_unit_spike_train(unit)
+                qual = 0
+                # mean WF @ Fs of recording
+                # nblocked mwf_list = \
+                # nblocked  sorted_data.get_unit_property(unit, "template").T
+                print(f'unit_idx {unit_idx}', flush=True)
+                print(f't_ch_size {t_ch_size}', flush=True)
+                print(f'peak_channels {peak_channels[unit_idx]}', flush=True)
+                print(f'unit {unit}')
+                print(f'sh wfs[unit_idx][:, :, :] {wfs[unit_idx].shape}')
+                mwf_list =\
+                    np.mean(wfs[unit_idx][:, :, :],
+                            axis=0)
+
+                max_channel = peak_channels[unit_idx]
+                #  try:
+                # print("Skipped i ", i, " unit ", unit)
+                # nblocked       print("unit_idx ", unit_idx, " unit ", unit,
+                # nblocked             " max_channel : ", tmp_max_channel,
+                # nblocked             " ", max_channel)
+                print("unit_idx ", unit_idx, " unit ", unit,
+                      " max_channel : ", max_channel)
+                # mwf = [row[tmp_max_channel] for row in mwf_list]
+                # mwf =\
+                #     np.mean(wfs[unit_idx][:, :, peak_channels[unit_idx]],
+                #             axis=0)
+                print(f'max_channel {max_channel} t_ch_size {t_ch_size}'
+                      f'get_group_value '
+                      f'{get_group_value(max_channel, t_ch_size)}')
+                mwf =\
+                    np.mean(wfs[unit_idx][:, :, get_group_value(max_channel,
+                                                                t_ch_size)],
+                            axis=0)
+
+                t = np.arange(0, len(mwf))
+                _, mwfs = ntk.data_intpl(t, mwf, 3, intpl_kind='cubic')
+
+                # def __init__(self, sp_c, sp_t, qual, mwf, mwfs, max_channel,
+                #              fs=25000, start_time=0, end_time=12 * 60 * 60,
+                #              mwft=None,
+                #              sex=None, birthday=None, species=None):
+                if ((len(file_datetime_list) == 2) and
+                        (len(ecube_time_list) == 2)):
+                    if (amps is not None):
+                        # print("amps not None")
+                        # print("amps not None")
+                        # print("mean amps ", unit_idx)
+
+                        # tmp_mean_amps = np.mean(np.asarray(amps[unit_idx]))
+                        tmp_mean_amps = \
+                            np.mean(np.abs(amps[amps_keys[unit_idx]]))
+                        if tmp_mean_amps >= min_amps:
+                            print(f'unit_idx {unit_idx} unit {unit}',
+                                  flush=True)
+                            print(f'amps_keys {amps_keys}', flush=True)
+                            print(f'len wf_b {len(wf_b)} len wf_e {len(wf_e)}',
+                                  flush=True)
+                            print("mean amps ", unit_idx, " ", tmp_mean_amps,
+                                  flush=True)
+                            if ((wf_b is not None) and (wf_e is not None)):
+                                print("1.11111", flush=True)
+                                n.append(mb.Neuron(sp_c, sp_t, qual, mwf,
+                                         mwfs, [max_channel],
+                                         fs=fs,
+                                         start_time=start_time,
+                                         end_time=end_time,
+                                         mwft=mwf_list,
+                                         rstart_time=str(file_datetime_list
+                                                         [0]),
+                                         rend_time=str(file_datetime_list[1]),
+                                         estart_time=int(ecube_time_list
+                                                         [0]),
+                                         eend_time=int(ecube_time_list
+                                                       [1]),
+                                         sp_amp=np.abs(
+                                                amps[amps_keys[unit_idx]]
+                                                ),
+                                         wf_b=np.asarray(wf_b[unit_idx]).T,
+                                         wf_e=np.asarray(wf_e[unit_idx]).T,
+                                         sex=sex, birthday=birthday,
+                                         species=species,
+                                         animal_name=animal_name,
+                                         region_loc=region_loc,
+                                         genotype=genotype,
+                                         expt_cond=expt_cond))
+                            #
+                            # nblocked elif (wf_b is None):
+                            # nblocked     n.append(mb.Neuron(sp_c, sp_t, qual,
+                            # nblocked       mwf,
+                            # nblocked       mwfs, max_channel,
+                            # nblocked       fs=fs,
+                            # nblocked       start_time=start_time,
+                            # nblocked       end_time=end_time,
+                            # nblocked       mwft=mwf_list,
+                            # nblocked       rstart_time=str(file_datetime_list
+                            # nblocked                       [0]),
+                            # nblocked    rend_time=str(file_datetime_list[1]),
+                            # nblocked       estart_time=int(ecube_time_list
+                            # nblocked                       [0]),
+                            # nblocked       eend_time=int(ecube_time_list
+                            # nblocked                     [1]),
+                            # nblocked       sp_amp=amps[unit_idx],
+                            # nblocked       sex=sex, birthday=birthday,
+                            # nblocked       species=species,
+                            # nblocked       animal_name=animal_name,
+                            # nblocked       region_loc=region_loc,
+                            # nblocked       genotype=genotype,
+                            # nblocked       expt_cond=expt_cond))
+
+                        else:
+                            print("Not added mean amps ", unit_idx, " ",
+                                  tmp_mean_amps, flush=True)
+
+                    # nblocked elif (amps is None):
+                    # nblocked     n.append(mb.Neuron(sp_c, sp_t, qual, mwf,
+                    # nblocked          mwfs, [max_channel],
+                    # nblocked          fs=fs,
+                    # nblocked          start_time=start_time,
+                    # nblocked          end_time=end_time,
+                    # nblocked          mwft=mwf_list,
+                    # nblocked          rstart_time=str(file_datetime_list[0]),
+                    # nblocked          rend_time=str(file_datetime_list[1]),
+                    # nblocked          estart_time=int(ecube_time_list[0]),
+                    # nblocked          eend_time=int(ecube_time_list[1]),
+                    # nblocked          sex=sex, birthday=birthday,
+                    # nblocked          species=species,
+                    # nblocked          animal_name=animal_name,
+                    # nblocked          region_loc=region_loc,
+                    # nblocked          genotype=genotype,
+                    # nblocked          expt_cond=expt_cond))
+                elif ((len(file_datetime_list) == 2) and
+                        (len(ecube_time_list) == 0)):
+                    print("2.11111", flush=True)
+                    n.append(mb.Neuron(sp_c, sp_t, qual, mwf,
+                             mwfs, [max_channel],
+                             fs=fs,
+                             start_time=start_time, end_time=end_time,
+                             mwft=mwf_list,
+                             rstart_time=str(file_datetime_list[0]),
+                             rend_time=str(file_datetime_list[1]),
+                             sex=sex, birthday=birthday, species=species,
+                             animal_name=animal_name,
+                             region_loc=region_loc,
+                             genotype=genotype,
+                             expt_cond=expt_cond))
+                else:
+                    print("3.11111", flush=True)
+                    n.append(mb.Neuron(sp_c, sp_t, qual, mwf,
+                             mwfs, [max_channel],
+                             fs=fs,
+                             start_time=start_time, end_time=end_time,
+                             mwft=mwf_list,
+                             sex=sex, birthday=birthday, species=species,
+                             animal_name=animal_name,
+                             region_loc=region_loc))
+                #  except:
+                #  pdb.set_trace()
+            elif len(filt) > 0:
+                print("sorry, we don't have qualities set yet, "
+                      "run again with no filter")
+
+    print(f'Found {len(n)} neurons\n')
+    if lskipautoqual is None:
+        if op.exists(model_file) and op.isfile(model_file):
+            print("model_file was used ", model_file, flush=True)
+            mb.autoqual(n, model_file)
+        else:
+            print("Model_file {} does not exists".format(model_file),
+                  flush=True)
+            print("neurons[0].quality not calculated")
+
+    return n
 
 
 def mbt_sorter_interface_out(
@@ -124,6 +394,12 @@ def mbt_sorter_interface_out(
     rl = glob.glob('*_amplitudes_group0.npy')[0]
     amps = mb.load_np(rl, lpickle=True)
 
+    rl = glob.glob('*_waveforms_list_group0.npy')[0]
+    wfs = mb.load_np(rl, lpickle=True)
+
+    rl = glob.glob('*_peak_channel_group0.npy')[0]
+    peak_channels = mb.load_np(rl, lpickle=True)
+
     rl = glob.glob('*_b_waveforms_group0.npy')[0]
     wf_b = mb.load_np(rl, lpickle=True)
 
@@ -138,33 +414,36 @@ def mbt_sorter_interface_out(
         t_ch_size = 4
         print("Setting t_ch_size to 4")
 
-    cells = mb.siout(sorted_data, noflylist, rec_time,
-                     file_datetime_list, ecube_time_list,
-                     amps=amps,
-                     wf_b=wf_b, wf_e=wf_e,
-                     filt=filt,
-                     t_ch_size=t_ch_size,
-                     model_file=model_file,
-                     sex=sex, birthday=birthday, species=species,
-                     animal_name=animal_name,
-                     region_loc=region_loc,
-                     genotype=genotype,
-                     expt_cond=expt_cond)
-    # def siout(sorted_data, noflylist, rec_time,
-    #           file_datetime_list, ecube_time_list,
-    #           wfs=None,
-    #           peak_channels=None,
-    #           amps=None,
-    #           wf_b=None, wf_e=None,
-    #           filt=None,
-    #           t_ch_size=None,
-    #           model_file='/media/HlabShare/models/xgboost_autoqual_prob',
-    #           sex=None, birthday=None, species=None,
-    #           animal_name=None,
-    #           region_loc=None,
-    #           genotype=None,
-    #           expt_cond=None,
-    #           lskipautoqual=None,
-    #           min_amps=10)
+    # cells = mb.siout(sorted_data, noflylist, rec_time,
+    #                  file_datetime_list, ecube_time_list,
+    #                  amps=amps,
+    #                  wf_b=wf_b, wf_e=wf_e,
+    #                  filt=filt,
+    #                  t_ch_size=t_ch_size,
+    #                  model_file=model_file,
+    #                  sex=sex, birthday=birthday, species=species,
+    #                  animal_name=animal_name,
+    #                  region_loc=region_loc,
+    #                  genotype=genotype,
+    #                  expt_cond=expt_cond)
+    cells = ms5out(sorted_data, noflylist, rec_time,
+                   file_datetime_list, ecube_time_list,
+                   wfs=wfs,
+                   peak_channels=peak_channels,
+                   amps=amps,
+                   wf_b=wf_b, wf_e=wf_e,
+                   filt=filt,
+                   t_ch_size=t_ch_size,
+                   model_file=model_file,
+                   sex=sex, birthday=birthday, species=species,
+                   animal_name=animal_name,
+                   region_loc=region_loc,
+                   genotype=genotype,
+                   expt_cond=expt_cond,
+                   lskipautoqual=None,
+                   min_amps=10)
+
+    # wfs=wfs,
+    # peak_channels=peak_channel,
 
     return cells
